f2117aed73c4dcdcabab694055ea9919935ecadb
Rafaella Nascimento <129559281+ella-stein@users.noreply.github.com>
2024-03-10 23:04:44 -0300

    modified:   src/app/app.component.ts 	modified:   src/app/app.routes.ts 	modified:   src/app/login/login.component.html 	modified:   src/app/login/login.component.ts 	modified:   src/app/registro/registro.component.html 	modified:   src/app/registro/registro.component.ts

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,16 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from './login/login.component';
+import { RegistroComponent } from './registro/registro.component';
 
 export const routes: Routes = [{
-    path:'', component: LoginComponent
-}];
+    path:'login', 
+    component: LoginComponent
+},
+{
+    path:'registro',
+    component:RegistroComponent
+},
+{
+    path: '', redirectTo:'login', pathMatch:'full'
+}
+];
--- src/app/registro/registro.component.ts
+++ src/app/registro/registro.component.ts
@@ -1,12 +1,21 @@
 import { Component } from '@angular/core';
+import { FormBuilder } from '@angular/forms';
+import { Validators } from '@angular/forms';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-registro',
   standalone: true,
-  imports: [],
+  imports: [RouterModule],
   templateUrl: './registro.component.html',
   styleUrl: './registro.component.css'
 })
 export class RegistroComponent {
+  
+  FormularioRegistro=this.fb.group({
+    
+  })
+
+  constructor(private fb: FormBuilder){}
 
 }
