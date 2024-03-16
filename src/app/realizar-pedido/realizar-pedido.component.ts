fade837d952952f3125e43dc347b101df217b772
JanainaNogueira <janainanogueira464@gmail.com>
2024-03-15 23:43:39 -0300

    feat: Cria card de descrição do pedido

--- src/app/realizar-pedido/realizar-pedido.component.ts
+++ src/app/realizar-pedido/realizar-pedido.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import {MatCardModule} from '@angular/material/card';
-import {MatIconModule} from  '@angular/material/icon' ;
+import {MatIconModule} from  '@angular/material/icon';
+import {MatDatepickerModule} from  '@angular/material/datepicker' ;
+import {MatNativeDateModule} from '@angular/material/core'
 
 @Component({
   selector: 'app-realizar-pedido',
   standalone: true,
-  imports: [MatCardModule, MatIconModule],
+  imports: [MatCardModule, MatIconModule, MatDatepickerModule, MatNativeDateModule],
   templateUrl: './realizar-pedido.component.html',
   styleUrl: './realizar-pedido.component.css'
 })
