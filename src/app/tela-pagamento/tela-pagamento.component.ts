17ed9ab5dc7ac0b5397e9b4652611647b5ecadc4
BeatrizHCor <92833059+BeatrizHCor@users.noreply.github.com>
2024-03-20 14:27:30 -0300

    Feat: Tela de Pagamento

---
+++ src/app/tela-pagamento/tela-pagamento.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-tela-pagamento',
+  standalone: true,
+  imports: [],
+  templateUrl: './tela-pagamento.component.html',
+  styleUrl: './tela-pagamento.component.css'
+})
+export class TelaPagamentoComponent {
+
+}
