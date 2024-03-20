c96a6188ced3fe13bfc04bea2dfd4c736ac42829
Rafaella Nascimento <129559281+ella-stein@users.noreply.github.com>
2024-03-20 00:36:37 -0300

    Feat: Cria path de Listar Pedido

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,6 +3,7 @@ import { LoginComponent } from './login/login.component';
 import { RegistroComponent } from './registro/registro.component';
 import { TelaClienteComponent } from './tela-cliente/tela-cliente.component';
 import { RealizarPedidoComponent } from './realizar-pedido/realizar-pedido.component';
+import { ListarPedidoComponent } from './listar-pedido/listar-pedido.component';
 
 export const routes: Routes = [{
     path:'login',
@@ -16,5 +17,6 @@ export const routes: Routes = [{
     path: '', redirectTo:'login', pathMatch:'full'
 },
 {path: 'home', component: TelaClienteComponent},
-{path: 'realizar-pedido', component: RealizarPedidoComponent}
+{path: 'realizar-pedido', component: RealizarPedidoComponent},
+{path: 'listar-pedido', component: ListarPedidoComponent}
 ];
