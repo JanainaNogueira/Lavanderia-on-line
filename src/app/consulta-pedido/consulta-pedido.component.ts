994dc9ca5204d43ee953a32ad397eee20ced51e6
BeatrizHCor <92833059+BeatrizHCor@users.noreply.github.com>
2024-03-20 15:11:25 -0300

    feat: Menu Lateral

--- src/app/consulta-pedido/consulta-pedido.component.ts
+++ src/app/consulta-pedido/consulta-pedido.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { MenuLateralComponent } from '../menu-lateral/menu-lateral.component';
 
 @Component({
   selector: 'app-consulta-pedido',
   standalone: true,
-  imports: [],
+  imports: [MenuLateralComponent],
   templateUrl: './consulta-pedido.component.html',
   styleUrl: './consulta-pedido.component.css'
 })
