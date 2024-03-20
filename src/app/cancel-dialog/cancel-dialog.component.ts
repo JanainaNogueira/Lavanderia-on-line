fe96c430b9397ebd57d9567b9bd050912bc4de88
BeatrizHCor <92833059+BeatrizHCor@users.noreply.github.com>
2024-03-20 16:37:00 -0300

    Cancelar Pedido

--- src/app/cancel-dialog/cancel-dialog.component.ts
+++ src/app/cancel-dialog/cancel-dialog.component.ts
@@ -1,12 +1,42 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {
+  MatDialog,
+  MatDialogRef,
+  MatDialogActions,
+  MatDialogClose,
+  MatDialogTitle,
+  MatDialogContent,
+} from '@angular/material/dialog';
+import {MatButtonModule} from '@angular/material/button';
 
 @Component({
-  selector: 'app-cancel-dialog',
+  selector: 'app-cancelDialog',
+  styleUrl: 'cancel-dialog.component.css',
+  templateUrl: 'cancel-dialog.component.html',
   standalone: true,
-  imports: [],
-  templateUrl: './cancel-dialog.component.html',
-  styleUrl: './cancel-dialog.component.css'
+  imports: [MatButtonModule],
 })
-export class CancelDialogComponent {
+export class CancelDialog {
+  constructor(public dialog: MatDialog) {}
 
+  openDialog(enterAnimationDuration: string, exitAnimationDuration: string,): void {
+    this.dialog.open(CancelDialogW, {
+      width: '25vw',
+      enterAnimationDuration,
+      exitAnimationDuration,
+    });
+  }
+}
+
+@Component({
+  selector: 'cancelDialogW',
+  templateUrl: 'cancel-dialog-Window.component.html',
+  standalone: true,
+  imports: [MatButtonModule, MatDialogActions, MatDialogClose, MatDialogTitle, MatDialogContent],
+})
+export class CancelDialogW {
+  constructor(public dialogRef: MatDialogRef<CancelDialogW>) {}
+  onYesClick(){
+    alert('pedido cancelado')
+  }
 }
--- src/app/tela-cliente/tela-cliente.component.ts
+++ src/app/tela-cliente/tela-cliente.component.ts
@@ -4,11 +4,12 @@ import { CommonModule } from '@angular/common';
 import { Router } from '@angular/router';
 import { MatCommonModule } from '@angular/material/core';
 import { MatButtonModule } from '@angular/material/button';
+import { CancelDialog } from '../cancel-dialog/cancel-dialog.component';
 
 @Component({
   selector: 'app-tela-cliente',
   standalone: true,
-  imports: [MenuLateralComponent, CommonModule, MatButtonModule, MatCommonModule],
+  imports: [MenuLateralComponent, CommonModule, MatButtonModule, MatCommonModule, CancelDialog],
   templateUrl: './tela-cliente.component.html',
   styleUrl: './tela-cliente.component.css'
 })
