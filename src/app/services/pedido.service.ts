7cf10f57ddb56aa7a823fb920857ee03cb3c72d3
JanainaNogueira <janainanogueira464@gmail.com>
2024-03-24 21:20:05 -0300

    Refactor: Adiciona dado de teste estatico

--- src/app/services/pedido.service.ts
+++ src/app/services/pedido.service.ts
@@ -6,7 +6,18 @@ import { Pedido, Roupa } from '../Pedido';
 export class PedidoService {
 
   constructor() { }
-  pedidos:Pedido[]=[];
+  pedidos:Pedido[]=[
+    {
+      id:97,
+      prazo:6,
+      roupas:[
+        {tipo: 'meia', tecido: Array(4), tempo: 1, quantidade: 1},
+        {tipo: 'camiseta', tecido: Array(4), tempo: 5, quantidade: 1},
+        {tipo: 'camisa', tecido: Array(4), tempo: 6, quantidade: 1}
+      ],
+      valor:30
+    }
+  ];
   addItem(valor:number,prazo:number,roupas:Roupa[]){
     const novoPedido:Pedido={
       id:Math.floor(Math.random() * 100) + 1,
