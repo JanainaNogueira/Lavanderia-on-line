e4f1ee7322a92f7571c7847a139ab8cf3c95ac46
Vanessa Bughay <vanessagaebler@gmail.com>
2024-03-28 18:36:04 -0300

    Fix: remoção de informações repetitivas e link para listagem de pedidos

--- src/app/consulta-pedido/consulta-pedido.component.ts
+++ src/app/consulta-pedido/consulta-pedido.component.ts
@@ -3,29 +3,36 @@ import { MenuLateralComponent } from '../menu-lateral/menu-lateral.component';
 import { PedidoService } from '../services/pedido.service';
 import { Pedido } from '../Pedido';
 import { CommonModule } from '@angular/common';
+import { MatCommonModule } from '@angular/material/core';
+import { MatButtonModule } from '@angular/material/button';
+import {MatInputModule} from '@angular/material/input';
+import { MatIconModule } from '@angular/material/icon';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-consulta-pedido',
   standalone: true,
-  imports: [MenuLateralComponent, CommonModule],
+  imports: [CommonModule,MatCommonModule,MatButtonModule,MatInputModule,MatIconModule,FormsModule, MenuLateralComponent],
   templateUrl: './consulta-pedido.component.html',
   styleUrl: './consulta-pedido.component.css'
 })
 export class ConsultaPedidoComponent implements OnInit {
   pedidos: Pedido[] = [];
   pedidoSelecionado: Pedido | null = null;
-  notFound: boolean = false;
   mostrarDetalhesPedido: boolean = false;
 
-  constructor(private pedidoService: PedidoService) { }
+  constructor(private pedidoService: PedidoService) { 
+    this.pedidos = this.pedidoService.pedidos;
+  }
 
   ngOnInit(): void {
     this.pedidos = this.pedidoService.pedidos;
-  }
+  } 
 
   procurar(idPedido: number): void {
     this.pedidoSelecionado = this.pedidos.find(pedido => pedido.id === idPedido) || null;
     this.mostrarDetalhesPedido = this.pedidoSelecionado !== null;
-    this.notFound = this.pedidoSelecionado === null; 
-  }
+    }
+
+
 }
