22464e1fe8329038ebac817b08395f22b34ca123
Rafaella Nascimento <129559281+ella-stein@users.noreply.github.com>
2024-03-28 19:03:50 -0300

    Update listar-pedido.component.ts
    Fix: Adequação a Service de Pedidos

--- src/app/listar-pedido/listar-pedido.component.ts
+++ src/app/listar-pedido/listar-pedido.component.ts
@@ -8,61 +8,74 @@ import { FormsModule } from '@angular/forms';
 import { MenuLateralComponent } from '../menu-lateral/menu-lateral.component';
 import { Router } from '@angular/router';
 import { CancelDialog } from '../cancel-dialog/cancel-dialog.component';
+import { PedidoService } from '../services/pedido.service';
+import { OnInit } from '@angular/core';
+import { Pedido } from '../Pedido';
+
 
 @Component({
   selector: 'app-listar-pedido',
   standalone: true,
-  imports: [CommonModule,MatCommonModule,MatButtonModule,MatInputModule,MatIconModule,FormsModule, MenuLateralComponent, CancelDialog],
+  imports: [CommonModule,MatCommonModule,MatButtonModule,MatInputModule,
+  MatIconModule,FormsModule, MenuLateralComponent, CancelDialog],
   templateUrl: './listar-pedido.component.html',
   styleUrl: './listar-pedido.component.css'
 })
 
-export class ListarPedidoComponent {
-  constructor(private router: Router) { }
+export class ListarPedidoComponent implements OnInit {
+  pedidos: Pedido[] = [];
+  num: any;
+
+  constructor(
+    private pedidoService: PedidoService,
+    private router: Router 
+  ) { }
+
   ngOnInit() {
+    this.getPedidos();
     this.ordenarDataHora();
   }
 
-  pedidos = [
-    { numero: '0001', status: 'Aguardando Pagamento', data: '01/03/2024', hora: '08:30', valor: '100,00' },
-    { numero: '0002', status: 'Em Aberto', data: '09/03/2024', hora: '09:45', valor: '350,00' },
-    { numero: '0003', status: 'Rejeitado/Cancelado', data: '12/03/2024', hora: '10:15', valor: '200,00' },
-    { numero: '0004', status: 'Recolhido', data: '10/03/2024', hora: '11:00', valor: '50,00' },
-    { numero: '0005', status: 'Pago', data: '17/03/2024', hora: '13:20', valor: '75,00' },
-    { numero: '0006', status: 'Finalizado', data: '09/03/2024', hora: '15:30', valor: '150,00' }
-  ];
+  getPedidos() {
+    this.pedidos = this.pedidoService.getPedidos();
+  }
 
-  pedidosFiltrados = [...this.pedidos];
-  num: string = '';
- 
   filtroStatus(status: string) {
     if (status === 'Todos') {
-      this.pedidosFiltrados = [...this.pedidos];
-      this.ordenarDataHora()
+      this.getPedidos();
     } else {
-      this.pedidosFiltrados = this.pedidos.filter(pedido => pedido.status === status); 
-
+      this.pedidos = this.pedidoService.getPedidosStatus(status);
+    }
+    this.ordenarDataHora();
   }
-}
 
   ordenarDataHora() {
-    this.pedidosFiltrados.sort((a, b) => {
-      const dataHoraA = `${a.data} ${a.hora}`;
-      const dataHoraB = `${b.data} ${b.hora}`;
-      
-      return dataHoraB.localeCompare(dataHoraA);
+    this.pedidos.sort((a, b) => {
+      const dataA = this.criarData(a.data, a.hora);
+      const dataB = this.criarData(b.data, b.hora);
+      return dataB.getTime() - dataA.getTime();
     });
   }
+ 
+  criarData(dataString: string, horaString: string): Date {
+    const [dia, mes, ano] = dataString.split('/');
+    const [hora, minuto] = horaString.split(':');
+    return new Date(parseInt(ano), parseInt(mes) - 1, parseInt(dia), parseInt(hora), parseInt(minuto));
+  }
+  
 
-
-  pesquisarPorNumero(num: string) {
-    if (num.trim() === '') {
-      this.pedidosFiltrados = [...this.pedidos];
+  pesquisarPorNumero(num: any) {
+    if (num === '' || num === null || num === undefined) {
+      this.getPedidos();
     } else {
-      this.pedidosFiltrados = this.pedidos.filter(pedido => pedido.numero.includes(num));
+      const pedidoId = parseInt(num, 10);
+      if (!isNaN(pedidoId)) {
+        this.pedidos = this.pedidoService.getPedidosID(pedidoId);
+        this.ordenarDataHora();
+      }
     }
   }
-
+  
   redirectPayment(num: string){
     this.router.navigateByUrl(`/payment/${num}`);
   }
