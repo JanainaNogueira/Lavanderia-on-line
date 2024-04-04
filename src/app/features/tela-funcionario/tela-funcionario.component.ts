c0b95d3d8b841c4864d5f1bf6d39cd5e50e9caca
Rafaella Nascimento <129559281+ella-stein@users.noreply.github.com>
2024-04-04 01:37:00 -0300

    Fix: Alteração para menu Administrativo

--- src/app/features/tela-funcionario/tela-funcionario.component.ts
+++ src/app/features/tela-funcionario/tela-funcionario.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { MenuLateralComponent } from '../../components/menu-lateral/menu-lateral.component';
+import { MenuAdminComponent } from '../../components/menu-admin/menu-admin.component';
 import { CommonModule } from '@angular/common';
 import { Router } from '@angular/router';
 import { MatCommonModule } from '@angular/material/core';
@@ -10,7 +10,7 @@ import { PedidoService } from '../../services/pedido.service';
 @Component({
   selector: 'app-tela-funcionario',
   standalone: true,
-  imports: [MenuLateralComponent, CommonModule, MatButtonModule, MatCommonModule, CancelDialog],
+  imports: [MenuAdminComponent, CommonModule, MatButtonModule, MatCommonModule, CancelDialog],
   templateUrl: './tela-funcionario.component.html',
   styleUrl: './tela-funcionario.component.css'
 })
