72597a5563be4092bb6fed2c34b6e50a30a53350
Rafaella Nascimento <129559281+ella-stein@users.noreply.github.com>
2024-04-04 01:29:57 -0300

    Fix: Alteração de padrão Data/Hora

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,9 +1,10 @@
 import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
+import { MAT_DATE_LOCALE } from '@angular/material/core';
 
 import { routes } from './app.routes';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), provideAnimationsAsync('noop')]
+  providers: [provideRouter(routes), provideAnimationsAsync('noop'),{provide:MAT_DATE_LOCALE,useValue:'pt-BR'}]
 };
