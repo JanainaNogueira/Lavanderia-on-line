02d9c7f5b0eec7328be806dc6260ac31470429aa
BeatrizHCor <92833059+BeatrizHCor@users.noreply.github.com>
2024-07-08 11:53:46 -0300

    feat: Email validator Directive

--- src/app/features/login/login.component.ts
+++ src/app/features/login/login.component.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
-import { FormBuilder,Validators } from '@angular/forms';
+import { FormBuilder,FormsModule,Validators } from '@angular/forms';
 import { ReactiveFormsModule } from '@angular/forms';
 import { RouterModule} from '@angular/router';
 import { CommonModule } from '@angular/common';
+import { EmailDirective } from '../../shared/directive/email.directive';
+import {Cliente} from '../../Cliente'
 
 
 
@@ -10,25 +12,17 @@ import { CommonModule } from '@angular/common';
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [ReactiveFormsModule,RouterModule,CommonModule],
+  imports: [ReactiveFormsModule,RouterModule,CommonModule, EmailDirective, FormsModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
-FormularioLogin = this.fb.group({
-    email: ['', [Validators.required, Validators.email]],
-    senha: ['', [Validators.required, Validators.maxLength(4), Validators.pattern('^[0-9]*$')]]
-  });
+  public cliente = {email: '', senha: -1};
+  constructor () { 
 
-  constructor (private fb: FormBuilder) { }
+  }
       
-    get email () {
-        return this.FormularioLogin.controls['email'];
-      }
-    
-    get senha () {
-        return this.FormularioLogin.controls['senha'];
-      }
+  
   
   }
 
--- src/app/shared/directive/email.directive.ts
+++ src/app/shared/directive/email.directive.ts
@@ -1,11 +1,30 @@
 import { Directive } from '@angular/core';
+import { Validator, NG_VALIDATORS, FormControl, ValidationErrors, AbstractControl } from '@angular/forms';
 
-@Directive({
-  selector: '[appEmail]',
-  standalone: true
-})
-export class EmailDirective {
+
+  @Directive({
+    selector: '[EmailValid]',
+    standalone: true,
+    providers: [{
+    provide: NG_VALIDATORS,
+    useExisting: EmailDirective,
+    multi: true
+    }]
+    })
+
+export class EmailDirective implements Validator {
 
   constructor() { }
+  validate(c: FormControl): ValidationErrors | null {
+    let email: string = c.value;
+    const emailRegex = new RegExp('[A-Za-z0-9._%-]+@[A-Za-z0-9._%-]+\\.[a-z]{2,3}', 'g');
+    const valid = emailRegex.test(email);
+    console.log(valid)
+    console.log(email)
+    if(email == (null || ""))
+    {
+      return {'EmailValid': true, 'requiredValue': 'Email'}
+    } else return null
+  }
 
 }
