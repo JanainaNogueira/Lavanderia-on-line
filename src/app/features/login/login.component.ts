2e077f8803b07c3b54a92a5669c5b5d7a7556f90
BeatrizHCor <92833059+BeatrizHCor@users.noreply.github.com>
2024-07-08 14:44:03 -0300

    feat: Numeric Validator

--- src/app/features/login/login.component.ts
+++ src/app/features/login/login.component.ts
@@ -4,15 +4,14 @@ import { ReactiveFormsModule } from '@angular/forms';
 import { RouterModule} from '@angular/router';
 import { CommonModule } from '@angular/common';
 import { EmailDirective } from '../../shared/directive/email.directive';
-import {Cliente} from '../../Cliente'
-
+import { NumericoDirective } from '../../shared/directive/numerico.directive';
 
 
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [ReactiveFormsModule,RouterModule,CommonModule, EmailDirective, FormsModule],
+  imports: [ReactiveFormsModule,RouterModule,CommonModule, EmailDirective, FormsModule, NumericoDirective],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
---
+++ src/app/shared/directive/numerico.directive.ts
@@ -0,0 +1,26 @@
+import { Directive } from '@angular/core';
+import { Validator, NG_VALIDATORS, FormControl, ValidationErrors, AbstractControl } from '@angular/forms';
+
+
+@Directive({
+  selector: '[invalidNumeric]',
+  standalone: true,
+  providers: [{
+    provide: NG_VALIDATORS,
+    useExisting: NumericoDirective,
+    multi: true
+    }]
+})
+export class NumericoDirective implements Validator{
+
+  constructor() { }
+  validate(c: FormControl): ValidationErrors | null {
+    let value = Number(c.value)
+    console.log(value)
+    if(isNaN(value))
+    {
+      return {'invalidNumeric': true}
+    } else return null
+  }
+
+}
