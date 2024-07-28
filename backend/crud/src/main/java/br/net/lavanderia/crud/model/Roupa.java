e47d4db451fa4e724453192a16eb05244b986221
JanainaNogueira <janainanogueira464@gmail.com>
2024-07-28 18:58:29 -0300

    feat: Criado rest de roupa

--- backend/crud/src/main/java/br/net/lavanderia/crud/model/Roupa.java
+++ backend/crud/src/main/java/br/net/lavanderia/crud/model/Roupa.java
@@ -1,5 +1,27 @@
 package br.net.lavanderia.crud.model;
 
 public class Roupa {
-    
+  private String tipo;
+  private int tempo;
+  public Roupa () {}
+  public Roupa (String tipo, int tempo) {
+      this.tipo = tipo;
+      this.tempo = tempo;
+  }
+
+  public String getTipo(){
+      return this.tipo;
+  }
+
+  public void setTipo( String tipo){
+      this.tipo = tipo;
+  }
+
+  public int getTempo(){
+      return this.tempo;
+  }
+
+  public void setTempo( int tempo){
+      this.tempo = tempo;
+  }
 }
