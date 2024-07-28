d2da81a01d6dbde7064884e966dbad77cce39ce8
JanainaNogueira <janainanogueira464@gmail.com>
2024-07-28 14:09:06 -0300

    feat: Criado projeto para o backend

---
+++ backend/crud/src/test/java/br/net/lavanderia/crud/CrudApplicationTests.java
@@ -0,0 +1,13 @@
+package br.net.lavanderia.crud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
