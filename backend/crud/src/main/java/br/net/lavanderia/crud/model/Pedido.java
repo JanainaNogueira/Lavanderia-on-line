7cbc75da5641f91b4078f67e1c041a2fb7e46525
JanainaNogueira <janainanogueira464@gmail.com>
2024-07-28 21:55:43 -0300

    feat:Criado rest de pedido

--- backend/crud/src/main/java/br/net/lavanderia/crud/model/Pedido.java
+++ backend/crud/src/main/java/br/net/lavanderia/crud/model/Pedido.java
@@ -1,5 +1,93 @@
 package br.net.lavanderia.crud.model;
 
+import java.util.List;
+
 public class Pedido {
-    
+    private int id;
+    private double valor;
+    private int prazo;
+    private List<ItemPedido> roupas;
+    private String hora;
+    private String status;
+    private String data;
+    private int clienteId;
+
+    // Construtor padrão
+    public Pedido() {}
+
+    // Construtor com parâmetros
+    public Pedido(int id, double valor, int prazo, List<ItemPedido> roupas, String hora, String status, String data, int clienteId) {
+        this.id = id;
+        this.valor = valor;
+        this.prazo = prazo;
+        this.roupas = roupas;
+        this.hora = hora;
+        this.status = status;
+        this.data = data;
+        this.clienteId = clienteId;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public double getValor() {
+        return valor;
+    }
+
+    public void setValor(double valor) {
+        this.valor = valor;
+    }
+
+    public int getPrazo() {
+        return prazo;
+    }
+
+    public void setPrazo(int prazo) {
+        this.prazo = prazo;
+    }
+
+    public List<ItemPedido> getRoupas() {
+        return roupas;
+    }
+
+    public void setRoupas(List<ItemPedido> roupas) {
+        this.roupas = roupas;
+    }
+
+    public String getHora() {
+        return hora;
+    }
+
+    public void setHora(String hora) {
+        this.hora = hora;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public String getData() {
+        return data;
+    }
+
+    public void setData(String data) {
+        this.data = data;
+    }
+
+    public int getClienteId() {
+        return clienteId;
+    }
+
+    public void setClienteId(int clienteId) {
+        this.clienteId = clienteId;
+    }
 }
