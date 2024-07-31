9ce5ab9d2c341f38203a3d08ff10900ed25855b4
Rafaella Nascimento <129559281+ella-stein@users.noreply.github.com>
2024-07-31 00:05:09 -0300

    Fix: Remover Imports não usados

--- backend/crud/src/main/java/br/net/lavanderia/crud/rest/FuncionarioREST.java
+++ backend/crud/src/main/java/br/net/lavanderia/crud/rest/FuncionarioREST.java
@@ -1,8 +1,7 @@
 package br.net.lavanderia.crud.rest;
 
-import java.util.ArrayList;
+
 import java.util.List;
-import java.util.Comparator;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
--- backend/crud/src/main/java/br/net/lavanderia/crud/rest/RoupaREST.java
+++ backend/crud/src/main/java/br/net/lavanderia/crud/rest/RoupaREST.java
@@ -1,6 +1,5 @@
 package br.net.lavanderia.crud.rest;
 
-import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
