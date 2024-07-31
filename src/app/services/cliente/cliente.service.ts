b80618fd15f9ce04488537f7d5d4e7fda2c6f6d6
vanessabughay <vanessagaebler@gmail.com>
2024-07-30 22:15:53 -0300

    fix: adaptação do service de cliente para conectar com o back

--- src/app/services/cliente/cliente.service.ts
+++ src/app/services/cliente/cliente.service.ts
@@ -1,12 +1,27 @@
 import { EmailService } from '../email.service';
 import { Injectable } from '@angular/core';
-import {Cliente } from '../../Cliente';
+import { Cliente } from '../../Cliente';
+import { HttpClient, HttpHeaders, HttpResponse } from '@angular/common/http';
+import { catchError, Observable, throwError, map, of } from 'rxjs';
+
+
 @Injectable({
   providedIn: 'root'
 })
 export class ClienteService {
+  BASE_URL = "http://localhost:8080/Cliente";
+  
+  httpOptions = {
+    observe: "response" as "response",
+    headers: new HttpHeaders({
+    'Content-Type': 'application/json'
+    })
+    };
 
-  constructor(private emailService: EmailService) { }
+  constructor(
+    private emailService: EmailService,
+    private httpClient: HttpClient
+    ) { }
   private clientes:Cliente[]=[
     {
       id: 1,
@@ -37,25 +52,46 @@ export class ClienteService {
     },
   ];
 
-  CreateCliente(nome:string, email: string,cpf: string,endereco: string,telefone: string){
-    const novoCliente:Cliente={
-      id:Math.round(Math.random()*1000000),
-      nome:nome,
+  inserir(cliente: Cliente): Observable<Cliente | null> {
+    return this.httpClient.post<Cliente>(this.BASE_URL, 
+      JSON.stringify(cliente), 
+      this.httpOptions).pipe(
+        map((resp: HttpResponse<Cliente>) => {
+          if (resp.status==201) {
+            return resp.body;
+          }
+          else {
+            return null;
+          }
+        }),
+        catchError((err, caught) => {
+          return throwError(() => err);
+        })
+      );
+  }
+
+  CreateCliente(nome: string, email: string, cpf: string, endereco: string, telefone: string): Observable<Cliente | null> | void {
+    const novoCliente: Cliente = {
+      id: Math.round(Math.random() * 1000000),
+      nome: nome,
       email: email,
       cpf: cpf,
       endereço: endereco,
       telefone: telefone,
       senha: Math.floor(1000 + Math.random() * 9000).toString()
-    }
+    };
+
     const exists = this.clientes.some(cliente => cliente.cpf === cpf);
     if (exists) {
       return;
-    }else{
-      this.clientes.push(novoCliente)
-      this.sendEmail(novoCliente)
-      console.log(this.clientes)
+    } else {
+      this.clientes.push(novoCliente);
+      this.sendEmail(novoCliente);
+      console.log(this.clientes);
+      return this.inserir(novoCliente);
     }
   }
+
   sendEmail(cliente:Cliente){
     this.emailService.sendEmail({
       to_name: cliente.nome,
@@ -69,11 +105,50 @@ export class ClienteService {
       console.error('Erro ao enviar email:', error);
     });
   }
-  getClientes(): Cliente[] {
-    return this.clientes;
-  }
-  getClientebyId(id: number): Cliente | undefined{
-    return this.clientes.find(c => c.id === id)
+  getClientes(): Observable<Cliente[] | null> {
+    return this.httpClient.get<Cliente[]>(
+      this.BASE_URL, 
+      this.httpOptions).pipe(
+        map((resp: HttpResponse<Cliente[]>) => {
+          if (resp.status==200) {
+            return resp.body;
+          }
+          else {
+            return [];
+          }
+        }),
+        catchError((err,caught) => {
+          if (err.status == 404) {
+            return of([]);
+          }
+          else {
+            return throwError(() => err);
+          }
+        })
+      );
+    }
+
+  getClientebyId(id: number): Observable<Cliente | null> {
+    return this.httpClient.get<Cliente>(
+      this.BASE_URL + "/" + id, 
+      this.httpOptions).pipe(
+        map((resp: HttpResponse<Cliente>) => {
+          if (resp.status==200) {
+            return resp.body;
+          }
+          else {
+            return null;
+          }
+        }),
+        catchError((err, caught) => {
+          if (err.status == 404) {
+            return of(null);
+          }
+          else {
+            return throwError(() => err);
+          } 
+        })
+      );
   }
 
   validateLogin(email: string, senha: string): boolean {
