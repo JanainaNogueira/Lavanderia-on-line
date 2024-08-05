07369e6b8e3c4f2f73b31201f3a0e8f482a15186
BeatrizHCor <92833059+BeatrizHCor@users.noreply.github.com>
2024-08-05 17:06:20 -0300

    feat: Listar adm

--- src/app/features/listar-adm/listar-adm.component.ts
+++ src/app/features/listar-adm/listar-adm.component.ts
@@ -1,89 +1,102 @@
-  import { CommonModule } from '@angular/common';
-  import { Component } from '@angular/core';
-  import { MatCommonModule, MatNativeDateModule } from '@angular/material/core';
-  import { MatButtonModule } from '@angular/material/button';
-  import { MatInputModule } from '@angular/material/input';
-  import { MatIconModule } from '@angular/material/icon';
-  import { FormsModule } from '@angular/forms';
-  import { MenuAdminComponent } from '../../components/menu-admin/menu-admin.component';
-  import { Router } from '@angular/router';
-  import { CancelDialog } from '../../components/cancel-dialog/cancel-dialog.component';
-  import { PedidoService } from '../../services/pedido.service';
-  import { OnInit } from '@angular/core';
-  import { Pedido } from '../../shared/models/Pedido';
-  import {MatDateRangeInput} from '@angular/material/datepicker';
-  import { MatDatepickerModule } from '@angular/material/datepicker';
-  import { MatDateRangePicker } from '@angular/material/datepicker';
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+import { MatCommonModule, MatNativeDateModule } from '@angular/material/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatInputModule } from '@angular/material/input';
+import { MatIconModule } from '@angular/material/icon';
+import { FormsModule } from '@angular/forms';
+import { MenuAdminComponent } from '../../components/menu-admin/menu-admin.component';
+import { Router } from '@angular/router';
+import { CancelDialog } from '../../components/cancel-dialog/cancel-dialog.component';
+import { PedidoService } from '../../services/pedido.service';
+import { OnInit } from '@angular/core';
+import { Pedido } from '../../shared/models/Pedido';
+import { MatDateRangeInput } from '@angular/material/datepicker';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatDateRangePicker } from '@angular/material/datepicker';
 
-
-  @Component({
-    selector: 'app-listar-adm',
-    standalone: true,
-    imports: [CommonModule,MatCommonModule,MatButtonModule,MatInputModule,
-      MatIconModule,FormsModule, MenuAdminComponent, CancelDialog,MatDateRangeInput,MatNativeDateModule,
-      MatDateRangePicker,MatDatepickerModule],
-    templateUrl: './listar-adm.component.html',
-    styleUrl: './listar-adm.component.css'
-  })
-
-  export class ListarAdmComponent {
+@Component({
+  selector: 'app-listar-adm',
+  standalone: true,
+  imports: [
+    CommonModule,
+    MatCommonModule,
+    MatButtonModule,
+    MatInputModule,
+    MatIconModule,
+    FormsModule,
+    MenuAdminComponent,
+    CancelDialog,
+    MatDateRangeInput,
+    MatNativeDateModule,
+    MatDateRangePicker,
+    MatDatepickerModule,
+  ],
+  templateUrl: './listar-adm.component.html',
+  styleUrl: './listar-adm.component.css',
+})
+export class ListarAdmComponent {
   pedidos: Pedido[] = [];
   pedidosOriginal: Pedido[] = [];
   num: any;
   dataInicio: Date;
   dataFim: Date;
-  statusAtual: string = "Todos"
+  statusAtual: string = 'Todos';
 
-  constructor(
-    private pedidoService: PedidoService,
-    private router: Router
-  ) { }
+  constructor(private pedidoService: PedidoService, private router: Router) {}
 
   ngOnInit() {
     this.getPedidos();
     this.ordenarDataHora();
+    this.pedidoService
+      .fetchPedidos()
+      .subscribe((p) =>
+        p ? (this.pedidos = p) : alert('Erro carregar pedidos')
+      );
   }
 
-  refetch(){
-    this.pedidos = this.pedidoService.getPedidosStatus("Em Aberto");
-  }
-  recolherPedido(id: number | undefined, pedido: Pedido){
-    if(!id)
-      return
-    this.pedidoService.updatePedidoStatus(id, "Recolhido", pedido)
+  recolherPedido(id: number | undefined, pedido: Pedido) {
+    if (!id) return;
+    this.pedidoService.updatePedidoStatus(id, 'Recolhido', pedido);
     this.getPedidos();
   }
-  confirmarLavagem(id: number | undefined, pedido: Pedido){
-    if(!id)
-      return
-    this.pedidoService.updatePedidoStatus(id, "Aguardando Pagamento", pedido)
+  confirmarLavagem(id: number | undefined, pedido: Pedido) {
+    if (!id) return;
+    this.pedidoService.updatePedidoStatus(id, 'Aguardando Pagamento', pedido);
     this.getPedidos();
   }
 
-  finalizarPedido(id: number | undefined, pedido: Pedido){
-    if(!id){
-      return
+  finalizarPedido(id: number | undefined, pedido: Pedido) {
+    if (!id) {
+      return;
     }
-    this.pedidoService.updatePedidoStatus(id, "Finalizado", pedido)
+    this.pedidoService.updatePedidoStatus(id, 'Finalizado', pedido);
     this.getPedidos();
   }
 
   getPedidos() {
-    this.pedidosOriginal = this.pedidoService.getPedidos();
-    this.filtroStatus(this.statusAtual)
+    this.pedidoService
+      .fetchPedidos()
+      .subscribe((p) =>
+        p ? (this.pedidosOriginal = p) : alert('Erro carregar pedidos')
+      );
   }
 
   filtroStatus(status: string) {
     this.statusAtual = status;
     if (status === 'Todos') {
       this.pedidos = [...this.pedidosOriginal];
-    }else if (status === 'Pedidos de Hoje') {
+    } else if (status === 'Pedidos de Hoje') {
       const hoje = new Date();
+      console.log(this.formataData(hoje));
       const hojeFormatado = this.formataData(hoje);
-      this.pedidos = this.pedidosOriginal.filter(pedido => pedido.data === hojeFormatado);
-    }
-    else {
-      this.pedidos = this.pedidoService.getPedidosStatus(status);
+      this.pedidos = this.pedidosOriginal.filter(
+        (pedido) => pedido.data === hojeFormatado
+      );
+    } else {
+      this.pedidos = this.pedidosOriginal.filter(
+        (p) => p.status === this.statusAtual
+      );
     }
     this.ordenarDataHora();
   }
@@ -96,11 +109,16 @@
     });
   }
 
-
   criarData(dataString: string, horaString: string): Date {
     const [dia, mes, ano] = dataString.split('/');
     const [hora, minuto] = horaString.split(':');
-    return new Date(parseInt(ano), parseInt(mes) - 1, parseInt(dia), parseInt(hora), parseInt(minuto));
+    return new Date(
+      parseInt(ano),
+      parseInt(mes) - 1,
+      parseInt(dia),
+      parseInt(hora),
+      parseInt(minuto)
+    );
   }
 
   pesquisarPorNumero(num: any) {
@@ -149,10 +167,18 @@
       console.log('Data Inicio:', this.dataInicio);
       console.log('Data Fim:', this.dataFim);
 
-      this.pedidos = this.pedidosOriginal.filter(pedido => {
+      this.pedidos = this.pedidosOriginal.filter((pedido) => {
         const pedidoData = new Date(this.formataDataPedido(pedido.data));
-        const isWithinRange = pedidoData >= inicioFormatted && pedidoData <= fimFormatted;
-        console.log('Pedido:', pedido.id, 'Data:', pedido.data, 'Within Range:', isWithinRange);
+        const isWithinRange =
+          pedidoData >= inicioFormatted && pedidoData <= fimFormatted;
+        console.log(
+          'Pedido:',
+          pedido.id,
+          'Data:',
+          pedido.data,
+          'Within Range:',
+          isWithinRange
+        );
         return isWithinRange;
       });
       this.pedidos.sort((a, b) => {
@@ -165,8 +191,8 @@
   }
 
   formataData(date: Date): string {
-    const day = String(date.getDate()).padStart(2, '0');
-    const month = String(date.getMonth() + 1).padStart(2, '0');
+    const day = String(date.getDate());
+    const month = String(date.getMonth() + 1);
     const year = date.getFullYear();
     return `${day}/${month}/${year}`;
   }
@@ -180,9 +206,8 @@
     this.filtroData();
   }
 
-  visualizarPedido(num:number | undefined){
-    if(!num)
-      return
+  visualizarPedido(num: number | undefined) {
+    if (!num) return;
     this.router.navigateByUrl(`/payment/${num}`);
   }
 }
