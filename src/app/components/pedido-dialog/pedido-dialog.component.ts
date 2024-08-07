6b79f1ed5577f3658ed89ad56187028badffdb93
BeatrizHCor <92833059+BeatrizHCor@users.noreply.github.com>
2024-08-07 19:58:41 -0300

    style: MatDialog Visualizar Pedido

--- src/app/components/pedido-dialog/pedido-dialog.component.ts
+++ src/app/components/pedido-dialog/pedido-dialog.component.ts
@@ -1,20 +1,25 @@
 import { Component, Inject, ViewEncapsulation } from '@angular/core';
-import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
+import {
+  MAT_DIALOG_DATA,
+  MatDialogRef,
+  MatDialogModule,
+} from '@angular/material/dialog';
 import { Pedido } from '../../shared/models/Pedido';
 import { CommonModule, NgFor, NgIf } from '@angular/common';
-
 
 @Component({
   selector: 'app-pedido-dialog',
   standalone: true,
-  imports: [NgIf,NgFor,CommonModule],
+  imports: [NgIf, NgFor, CommonModule, MatDialogModule],
   templateUrl: './pedido-dialog.component.html',
   styleUrl: './pedido-dialog.component.css',
-  encapsulation:ViewEncapsulation.None
+  encapsulation: ViewEncapsulation.None,
 })
 export class PedidoDialogComponent {
-  constructor(@Inject(MAT_DIALOG_DATA) public data: Pedido,
-  private dialogRef: MatDialogRef<PedidoDialogComponent> ) {}
+  constructor(
+    @Inject(MAT_DIALOG_DATA) public data: Pedido,
+    private dialogRef: MatDialogRef<PedidoDialogComponent>
+  ) {}
 
   closeDialog() {
     this.dialogRef.close();
